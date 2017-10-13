204fdbdd82d68c4de20ad9617219c19c01f2bcc1
Raethandres <raetthandres4qgamil.com>
2017-10-13 12:02:35 -0400

    cabios css y html

--- php/back.php
+++ php/back.php
@@ -14,7 +14,8 @@
 		{
 			$this->tipo=tipo;
 			$this->form=form;
-			var_dump(tipo);
+			echo $this->tipo;
+			// var_dump(tipo);
 			// fu=$this->func[$this->tipo];
 			// fu();
 		}
@@ -35,6 +36,16 @@
 
 	}
 
-$ss="true";
-echo($ss);
+if ($_POST) {
+	$reg=new Request("Post",$_POST);
+}elseif ($_GET) {
+	$reg=new Request("Get",$_GET);
+}elseif ($_PUT) {
+	$reg=new Request("Put",$_PUT);
+}elseif ($_DELETE) {
+	$reg=new Request("Delete",$_DELETE);
+}else{
+	echo "nada";
+}
+
 ?>
